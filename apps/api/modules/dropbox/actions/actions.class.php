58f85b88f31a4e4c104c40fff407cab209fc361e
手嶋守 <tejima@gmail.com>
2012-06-22 03:28:26 +0900

    community UPLOAD DOWNLOAD function

--- apps/api/modules/dropbox/actions/actions.class.php
+++ apps/api/modules/dropbox/actions/actions.class.php
@@ -10,12 +10,41 @@ class dropboxActions extends opJsonApiActions
     return $dropbox;
   }
 
+  public function executeDelete(sfWebRequest $request)
+  {
+    $path = $request->getParameter("path");
+    $dropbox = $this->getDropbox();
+    $delete_ok = false
+    if(preg_match('/^PNE\/m(\d+)/',$path,$match)){
+      $path_member_id = $match[1];
+      //member mode
+      $member_id = $this->getUser()->getMember()->getId();
+      if($path_member_id != $member_id){
+        return $this->renderJSON(array('status' => 'error','message' => 'you can delete only your own directary. ' . $path));
+      }
+      $delete_ok = true;
+    }
+    else if(preg_match('/^PNE\/c(\d+)/',$path,$match)){
+      $path_community_id = $match[1];
+      $community = Doctrine::getTable("Community")->find($community_id);
+      if(!$community->isAdmin($this->getUser()->getMember()->getId())){
+        return $this->renderJSON(array('status' => 'error','message' => 'only community_admin can delete community directary. ' . $path));
+      }
+      $delete_ok = true;
+    }
+    if($is_ok){
+      $response = $dropbox->delete($path);
+      return $this->renderJSON(array('status' => 'success','data' => $response));
+    }
+  }
+
   public function executeList(sfWebRequest $request)
   {
     $dropbox = $this->getDropbox();
     $response = $dropbox->getMetaData('/PNE/');
     return $this->renderJSON(array('status' => 'success','data' => $response));
   }
+
   public function executeFiles(sfWebRequest $request)
   {
     $path = $request->getParameter("path");
@@ -57,9 +86,33 @@ class dropboxActions extends opJsonApiActions
     if(!$filename){
       return $this->renderJSON(array('status' => 'error' ,'path' => $response, 'message' => "null file"));
     }
-    $dropbox = $this->getDropbox();
 
-    $response = $dropbox->putFile('/PNE/'.$filename, $_FILES['upfile']['tmp_name']);
+    $community_id = (int)$request->getParameter("community_id");
+    if((int)$community_id >= 1){
+      $community = Doctrine::getTable("Community")->find($community_id);
+      if(!$community->isPrivilegeBelong($this->getUser()->getMember()->getId())){
+        return $this->renderJSON(array('status' => 'error' ,'message' => "you are not this community member."));
+      }
+      
+      $dirname = '/PNE/c'. $community_id;
+    }else{
+
+      $dirname = '/PNE/c'. $community_id;
+      $filepath = '/PNE/m'. $this->getUser()->getMember()->getId() 
+    }
+   
+    //validate $filepath
+    if(!preg_match('/PNE\/[cm]\d+\/.*$')){
+      return $this->renderJSON(array('status' => 'error' ,'message' => "file path error."));
+    }
+ 
+    $dropbox = $this->getDropbox();
+    try{
+      $response = $dropbox->createFolder($dirname);
+    }catch(Exception $e){
+      error_log($e->toString(), 3, "/tmp/loglog"); 
+    }
+    $response = $dropbox->putFile($dirname .'/'. $_FILES['upfile']['tmp_name']);
     if($response === true){
       //$response = $dropbox->share('/PNE/'.$filename);
       return $this->renderJSON(array('status' => 'success' , 'message' => "file up success"));
