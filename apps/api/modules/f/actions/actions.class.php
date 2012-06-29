0bea329d81d611103b3b8f34d197f8c8088bf3ea
tejima <tejima@gmail.com>
2012-06-29 13:46:21 +0900

    ファイルデータ取得作成。

--- apps/api/modules/f/actions/actions.class.php
+++ apps/api/modules/f/actions/actions.class.php
@@ -25,4 +25,32 @@ class fActions extends opJsonApiActions
      
     return $this->renderText(print_r($file_list,true));
   }
+  public function executeFiles(sfWebRequest $request)
+  {
+    $path = $request->getParameter("path");
+    if(!$path){
+      $path = "/m1/1340943961FILENAME.txt";
+    }
+    //TODO アクセス権限管理
+
+    $file = Doctrine::getTable("File")->findOneByName($path);
+    
+    $filebin = $file->getFileBin();
+    $data = $filebin->getBin();
+
+    if(!$data){
+      return $this->renderJSON(array('status' => 'error','message' => "Dropbox file download error"));
+    }
+
+    $filename = substr($path,strpos($path,"/",1));
+
+    $finfo = new finfo(FILEINFO_MIME_TYPE);
+    $type = $finfo->buffer($data);
+    $this->getResponse()->setHttpHeader('Content-Type',$type);
+    //if(strpos($type,'application') !== FALSE || $type == "text/x-php"){
+      $this->getResponse()->setHttpHeader('Content-Disposition','attachment; filename="'.$filename.'"');
+    //}
+    return $this->renderText($data);
+  }
+
 }
