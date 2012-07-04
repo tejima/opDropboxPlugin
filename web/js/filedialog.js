4821e128284f5bf284967da139535f1d457b0026
tejima <tejima@gmail.com>
2012-07-04 18:27:42 +0900

    モーダル用の土台

---
+++ web/js/filedialog.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("#diary_body_buttonmenu").append('<a data-toggle="modal" href="#myModal" class="btn btn-filecoice">ファイル選択</a>');
+  $("#diary_body_buttonmenu").append('<a data-toggle="modal" href="#myModal" class="btn btn-fileup">アップロード</a>');
+
+});
+
+
+
+
+
+
+
